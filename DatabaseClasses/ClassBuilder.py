63005b81413f70fe153278885eae2284a8cd1b62
richardrod605 <35390761+richardrod605@users.noreply.github.com>
2019-10-30 16:05:07 -0700

    Create ClassBuilder.py

---
+++ DatabaseClasses/ClassBuilder.py
@@ -0,0 +1,91 @@
+import psycopg2
+# import os
+import getpass
+from configparser import ConfigParser
+
+
+#Taken from: http://www.postgresqltutorial.com/postgresql-python/connect/
+def config(filename='database.ini', section='postgresql'):
+    # create a parser
+    parser = ConfigParser()
+    # read config file
+    parser.read(filename)
+
+    # get section, default to postgresql
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+
+    return db #Returns a dictionary  with the config info for the psql database
+
+def ConnectToDatabaseServer():
+    try:
+        ConfigInfo = config()
+        print("Attemping to connect...")
+        Connection =psycopg2.connect(**ConfigInfo)
+        print("Connection successful...")
+
+        #Somthing cursor...
+        cur = Connection.cursor()
+        return cur
+
+        # print("PSQL database version:")
+        # cur.execute("SELECT version()")
+        # db_version = cur.fetchone()
+        # print(db_version)
+
+    except(Exception, psycopg2.DatabaseError) as error:
+        print("Connection error:")
+        print(error)
+    # finally:
+    #     if Connection is not None:
+    #         Connection.close()
+    #         print("Database connection closed")
+
+
+def CloseDatabaseConnection(cur):
+    try:
+        cur.close();
+        print("Database connection now closed")
+    except(Exception, psycopg2.DatabaseError) as error:
+        print("Closing connection error:")
+        print(error)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+cur = ConnectToDatabaseServer()
+
+
+#Make a class for only rainy day entries
+RainyDaysClass = [];
+cur.execute("SELECT * FROM weatherdata WHERE prcp > 1") #Pick entries with rain
+for record in cur:
+    RainyDaysClass.append(record)
+    # print(type(record))
+    # print(record)
+
+
+for j in RainyDaysClass:
+    print("here:", j)
+
+
+
+
+
+
+CloseDatabaseConnection(cur)
