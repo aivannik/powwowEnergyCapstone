66aa22cf5ab91d9756e786402f1e289e9dbdc812
richardrod605 <35390761+richardrod605@users.noreply.github.com>
2019-11-10 22:09:29 -0800

    Update ExtractVetorData.py

--- ExtractVectorData/ExtractVetorData.py
+++ ExtractVectorData/ExtractVetorData.py
@@ -1,4 +1,7 @@
 import ogr, osr
+import json
+import os
+
 
 
 PathName = "../../../PowWowData/landiq/ds2677.gdb"
@@ -18,5 +21,27 @@ Layer = DataSource.GetLayer()
 
 Layer.SetAttributeFilter("County = 'Santa Barbara'")
 
+#Convert from EPSG4326 to EPSG3310 (longitude and latitude)
+inputEPSG = 3310
+inSpatialRef = osr.SpatialReference()
+inSpatialRef.ImportFromEPSG(inputEPSG)
+outputEPSG = 4326 #For longitude and latitude
+outSpatialRef = osr.SpatialReference()
+outSpatialRef.ImportFromEPSG(outputEPSG)
+
+CoorTrans = osr.CoordinateTransformation(inSpatialRef, outSpatialRef)
+
+#Write a .json to folder for every vector and
+CurrentVector = 1;
 for Feature in Layer:
-    print(Feature.GetField("Crop2014"))
+    Geometry = Feature.GetGeometryRef()
+    Geometry.Transform(CoorTrans)
+    GeometryJson = json.loads(Geometry.ExportToJson())
+    Polygon = json.dumps(GeometryJson)
+
+    OutFileName = "SantaBarbaraVectors/Polygon" + str(CurrentVector) + ".json"
+    CurrentVector = CurrentVector + 1
+
+    os.makedirs(os.path.dirname(OutFileName), exist_ok=True)
+    with open(OutFileName, 'w') as f:
+        json.dump(Polygon, f)
