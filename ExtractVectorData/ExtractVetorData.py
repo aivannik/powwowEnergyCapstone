bab948081fd84b25cdddd56b59b51e2127028b09
richardrod605 <35390761+richardrod605@users.noreply.github.com>
2019-11-25 23:19:27 -0800

    Update ExtractVetorData.py
    Fixed issues with using .shp files instead of .gbd files

--- ExtractVectorData/ExtractVetorData.py
+++ ExtractVectorData/ExtractVetorData.py
@@ -2,6 +2,7 @@ import ogr, osr
 import json
 import os
 import psycopg2
+from psycopg2 import sql
 import getpass
 from configparser import ConfigParser
 
@@ -46,12 +47,59 @@ def ConnectToDatabaseServer():
 
 
 
+# Start here
+
+#PSQL preparation:
+# Make sure you have a "database.ini" file in you working directory
+# Also make sure your table as been set up in this format
+# ```CREATE TABLE sbvectors2 (id INT, crop VARCHAR(100), acres FLOAT, coordinates JSON);```
+
+#**********IMPORTANT**********
+#**********IMPORTANT**********
+#**********IMPORTANT**********
+#MAKE SURE THE cur.execute IS INSERTING TO THE CORRECT TABLE!!!!!!
 
 
 
-#Attept to open the .gdb file
-PathName = "../../../PowWowData/landiq/ds2677.gdb"
-driverName = "OpenFileGDB"
+# Enter filepath to .shp or .gdb
+PathName = "../../../PowWowData/landiq/ETaCrop2014/ETaCrop2014.shp"
+# PathName = "../../../PowWowData/landiq/ds2677.gdb"
+
+#Enter any filtering as an SQL command, ex "County = 'Santa Barbara' AND Acres < 0.05"
+Filter = ""
+# Filter = "Acres < 0.9"
+# Filter = "County = 'Santa Barbara' AND Acres < 0.05"
+# Filter = "County = 'Fresno'"
+
+
+#Specify which file format
+SHP = False
+GDB = False
+SHP = True
+# GDB = True
+
+
+
+
+
+
+
+
+
+
+
+
+
+if(SHP == GDB):
+    print("Specify file format")
+    exit()
+
+driverName = ""
+if(SHP == True):
+    driverName = "ESRI Shapefile"
+elif(GDB == True):
+    driverName = "OpenFileGDB"
+
 driver = ogr.GetDriverByName(driverName)
 if driver is None:
     print( "%s driver not available." % driverName)
@@ -62,11 +110,13 @@ if DataSource is None:
     print ("Could not open dataset")
     exit()
 Layer = DataSource.GetLayer()
+print("Dataset opened")
 
 #Filter the layer read from the dataset
-print("Now filtering...")
+print("Applying filtering...")
 # Layer.SetAttributeFilter("County = 'Santa Barbara' AND Acres < 0.05")
-Layer.SetAttributeFilter("County = 'Santa Barbara'")
+Layer.SetAttributeFilter(Filter)
+# Layer.SetAttributeFilter("County = 'Santa Barbara'")
 
 
 #Get the conversion from EPSG3310 to EPSG4326 (longitude and latitude)
@@ -76,16 +126,17 @@ inSpatialRef.ImportFromEPSG(inputEPSG)
 outputEPSG = 4326 #For longitude and latitude
 outSpatialRef = osr.SpatialReference()
 outSpatialRef.ImportFromEPSG(outputEPSG)
-
 CoorTrans = osr.CoordinateTransformation(inSpatialRef, outSpatialRef)
 
-#Connect to psql server
+
+# Connect to psql server
 Conn = ConnectToDatabaseServer()
 cur = Conn.cursor()
 
 IndexNumber = 0
 Acres = 0.0
 Crop = ""
+
 
 print("Cycling through features...")
 CurrentVector = 1;
@@ -98,25 +149,53 @@ for Feature in Layer:
     #Convert Geometry into a workabale json/dictionary
     GeometryJson = json.loads(Geometry.ExportToJson())
     #Remove z-axis and create a new json/dictionary
-    Polygon = []
-    for j in range(len(GeometryJson["coordinates"][0][0])):
-        Point = { "lng" : GeometryJson["coordinates"][0][0][j][0], "lat" : GeometryJson["coordinates"][0][0][j][1]}
-        Polygon.append(Point)
-    CoorJson = {"coordinates": Polygon}
+
+
+    if(SHP == True):
+        #NOTE: in later shapefiles, I removed the z dimension and the Geometry has changed!!!
+        if(len(GeometryJson["coordinates"][0][0]) != 2):
+            print("Error in geometry... Adding anyways, multiple polygons at OBJECTID: ", int(Feature.GetField("OBJECTID")))
+            print("Likely a vector entry with multiple polygons")
+
+        Polygon = []
+        for j in range(len(GeometryJson["coordinates"][0])):
+            Point = { "lng" : GeometryJson["coordinates"][0][j][0], "lat" : GeometryJson["coordinates"][0][j][1]}
+            Polygon.append(Point)
+            CoorJson = {"coordinates": Polygon}
+
+    else:
+        if(len(GeometryJson["coordinates"][0][0][0]) != 3):
+            print("Potential error in geometry... Adding anyways, multiple polygons at OBJECTID: ", Feature.GetFID())
+
+        Polygon = []
+        for j in range(len(GeometryJson["coordinates"][0][0])):
+            Point = { "lng" : GeometryJson["coordinates"][0][0][j][0], "lat" : GeometryJson["coordinates"][0][0][j][1]}
+            Polygon.append(Point)
+        CoorJson = {"coordinates": Polygon}
+
+
 
     #Add in some extra info
-    IndexNumber = Feature.GetFID()
-    Acres = Feature.GetField("Acres")
+    IndexNumber = 0;
+    if(SHP == True):
+        IndexNumber = int(Feature.GetField("OBJECTID"))
+    else:
+        IndexNumber = Feature.GetFID()
     Crop = Feature.GetField("Crop2014")
+    Acres = Feature.GetField("Acres")
 
 
     #Insert to the psql database
-    cur.execute("""INSERT INTO sbvectors2 ( id, crop, acres, coordinates ) VALUES (%s, %s, %s, %s) """, (IndexNumber, Crop, Acres, json.dumps(CoorJson)))
+    #IMPORTANT!
+    #MAKE SURE YOU'RE WRITING TO THE CORRECT TABLE
+    cur.execute("""INSERT INTO huron_delano_vectors ( id, crop, acres, coordinates ) VALUES (%s, %s, %s, %s) """, (IndexNumber, Crop, Acres, json.dumps(CoorJson)))
+    # cur.execute("""INSERT INTO sbvectors3 ( id, crop, acres, coordinates ) VALUES (%s, %s, %s, %s) """, (IndexNumber, Crop, Acres, json.dumps(CoorJson)))
 
 
 
 
 #Commit and close connections
+print("Script completed and entries placed in PSQL table")
 cur.close()
 Conn.commit()
 Conn.close()
